000e02038d2041df7ff88dfa8eff1684255927eb
guilledc <guill3rmo.11@gmail.com>
2022-11-18 14:01:27 -0300

    18/11 primer commit
    creando el proyecto

---
+++ Spring_DDD_Rest_Api/src/main/java/mx/com/ddd/spring/SpringDddRestApiApplication.java
@@ -0,0 +1,13 @@
+package mx.com.ddd.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDddRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDddRestApiApplication.class, args);
+	}
+
+}
---
+++ Spring_DDD_Rest_Api/src/test/java/mx/com/ddd/spring/SpringDddRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.com.ddd.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDddRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
