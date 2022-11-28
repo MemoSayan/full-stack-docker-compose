25bda1f605778e1291332872ac5c399fa7fb8ab2
guilledc <guill3rmo.11@gmail.com>
2022-11-28 13:12:29 -0300

    add target

---
+++ Spring_DDD_Rest_Api/target/classes/import.sql
@@ -0,0 +1,13 @@
+/* Populate tables */
+INSERT INTO employee (name, email, job_tittle, phone) VALUES('GUILLERMO DE LA CRUZ', 'guillermo@email.com',  'JAVA DEVELOPER', '889754446' );
+INSERT INTO employee (name, email, job_tittle, phone) VALUES('LEOANRDO DA VINCI', 'guillermo@email.com', 'UI/UX', '889754446');
+INSERT INTO employee (name, email, job_tittle, phone) VALUES('LINUS TOLVAR', 'guillermo@email.com', 'LINUS ADMIN', '6655437');
+
+
+
+
+
+
+
+
+
